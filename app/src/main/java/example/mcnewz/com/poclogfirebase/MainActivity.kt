650029b2ab6e1ad3e2199f9877f3e111c4d9cd1b
Manit Cholpinyo <manit.cholpinyo@krungsri.com>
2019-01-02 11:56:11 +0700

    Add firebase

--- app/src/main/java/example/mcnewz/com/poclogfirebase/MainActivity.kt
+++ app/src/main/java/example/mcnewz/com/poclogfirebase/MainActivity.kt
@@ -1,12 +1,21 @@
 package example.mcnewz.com.poclogfirebase
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.StringRes
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val context: Context = applicationContext
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        context.toast("Hello")
     }
+
+    fun Context.toast(resId: String, duration: Int = Toast.LENGTH_LONG) =
+        Toast.makeText(this, resId, duration).show()
+
 }
